c46d6fe43797573dbc04c663316b18a5eb5b05bb
Horacjusz <pawel.prus05@gmail.com>
2024-03-21 16:28:07 +0100

    Physics milestone 1

--- test.py
+++ test.py
@@ -3,9 +3,10 @@ import pygame
 import random
 import numpy as np
 from pygame.locals import *
-from copy import deepcopy
 
 
+# Ustawienie wymiarów okna
+# width, height = 2550, 1340
 width, height = 600,1000
 
 ELASTICITY = 1.001
@@ -18,14 +19,9 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 BLACK = (0, 0, 0)
-FLIPPER_HOR = width//10
-FLIPPER_VERT = height//20
 
 # Inicjalizacja Pygame
 pygame.init()
-# Ustawienie wymiarów okna
-# width, height = 2550, 1340
-width, height = 600,1000
 
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Pymunk Simulation")
@@ -56,7 +52,7 @@ def create_wall(start_pos = (random.randint(WALL_WIDTH,width-WALL_WIDTH),random.
 right_wall, right_wall_shape = create_wall(start_pos = (width - WALL_WIDTH//2, 0), end_pos = (width - WALL_WIDTH//2, height),color = WHITE)
 left_wall, left_wall_shape = create_wall(start_pos = (WALL_WIDTH//2, 0), end_pos = (WALL_WIDTH//2, height),color = WHITE)
 ceil, ceil_shape = create_wall(start_pos = (0, WALL_WIDTH//2), end_pos = (width, WALL_WIDTH//2),color = WHITE)
-floor, floor_shape = create_wall(start_pos = (0, height - WALL_WIDTH//2), end_pos = (width, height - WALL_WIDTH//2),color = WHITE)
+# floor, floor_shape = create_wall(start_pos = (0, height - WALL_WIDTH//2), end_pos = (width, height - WALL_WIDTH//2),color = WHITE)
 
 
 def create_ball(r = BALL_RADIUS, position = (random.randint(WALL_WIDTH,width-WALL_WIDTH),random.randint(WALL_WIDTH,height-WALL_WIDTH)), static = False, mass = None, color = (random.randint(0,255),random.randint(0,255),random.randint(0,255)), elasticity = ELASTICITY) :
@@ -78,9 +74,18 @@ def create_ball(r = BALL_RADIUS, position = (random.randint(WALL_WIDTH,width-WAL
     
 
 for i in range(NO_BALLS) :
-    
     ball,ball_shape = create_ball(r = BALL_RADIUS, position = (random.randint(WALL_WIDTH,width-WALL_WIDTH),random.randint(WALL_WIDTH,height-WALL_WIDTH)), static = False, mass = None, color = (random.randint(0,255),random.randint(0,255),random.randint(0,255)), elasticity = ELASTICITY)
 
+def draw_circle(body, shape) :
+    pos = body.position
+    radius = shape.radius
+    pygame.draw.circle(screen, object_colors[shape], (int(pos.x), int(pos.y)), int(radius), 0)
+    
+def draw_segment(body,shape) :
+    a = shape.a
+    b = shape.b
+    w = int(shape.radius)
+    pygame.draw.line(screen, WHITE, a, b, 2*w + 1)
 
 # Definicja funkcji renderującej tekst
 def draw_text(surface, text, pos, color, font_size=24):
@@ -141,7 +146,6 @@ while running:
         for shape in body.shapes:
             if isinstance(shape, pymunk.Circle):
                 pos = body.position
-                radius = shape.radius
                 if not inside(pos) :
                     space.remove(body)
                     print("REMOVED")
@@ -149,12 +153,9 @@ while running:
                     if removed == NO_BALLS :
                         print("ALL GONE")
                         running = False
-                pygame.draw.circle(screen, object_colors[shape], (int(pos.x), int(pos.y)), int(radius), 0)
+                draw_circle(body,shape)
             elif isinstance(shape, pymunk.Segment):
-                a = shape.a
-                b = shape.b
-                w = int(shape.radius)
-                pygame.draw.line(screen, WHITE, a, b, 2*w + 1)
+                draw_segment(body,shape)
                 
     # Wyświetlanie licznika FPS
     draw_text(screen, f"FPS: {int(clock.get_fps())}", (0,0), BLACK)
